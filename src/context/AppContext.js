ae8d9833c1a82233c06e4850a3e3a39ce9e1470e
Pulkit Gupta <pulkitgupta64@gmail.com>
2020-07-16 23:55:46 +0530

    Changed Link

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,7 +5,7 @@ const AppContext = React.createContext();
 export const AppProvider = function({children}){
 
     const [address,setAddress] = useState("")
-    const [ngurl,setngurl] = useState('http://d99ea0edfe41.ngrok.io') 
+    const [ngurl,setngurl] = useState('http://52.70.136.68:4001') 
     return <AppContext.Provider value={{address ,setAddress,ngurl}}>
 	{children}
 	</AppContext.Provider>
--- src/screens/Notification.js
+++ src/screens/Notification.js
@@ -1,39 +1,13 @@
-// import React, { Component } from 'react';
-// import { Notifications, Notification } from 'react-native-notifications';
-
-// const NotificationScreen = (props) => {
-//     Notifications.registerRemoteNotifications();
-//     Notifications.events().registerNotificationReceived((notification = Notification, completion) => {
-//         console.log(`Notification received in foreground: ${notification.title} : ${notification.body}`);
-//         completion({ alert: false, sound: false, badge: false });
-//     });
-
-//     Notifications.events().registerRemoteNotificationOpened((notification = Notification, completion) => {
-//         console.log(`Notification opened: ${notification.payload}`);
-//         completion();
-//     });
-// };
-
-// const styles = StyleSheet.create({
-
-// });
-
-// export default NotificationScreen;
-
-import React,{useContext,useState} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React,{useContext} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
 import { Permissions, Notifications } from 'expo';
 import { Button,Header } from 'react-native-elements';
 import AppContext from '../context/AppContext'
 
-
-
 const NotificationScreen = function({navigation}){
-
 
     console.log("NOTIFICATION SCREEN")
     const {address, setAddress,ngurl} = useContext(AppContext);
-   
     return(
 
         <View>
@@ -49,11 +23,10 @@ const NotificationScreen = function({navigation}){
             <View style ={{marginHorizontal: 20,marginVertical: 20}}>
                 <Text>Name: {navigation.state.params.name}</Text>
                 <Text>Mobile Number: {navigation.state.params.mobile}</Text>
-                {/* <Text>{navigation.state.params.address}</Text>  */}
                 <Text>Vehicle Number: {navigation.state.params.vehicle}</Text>
             </View>
 
-            <Text style = {{marginTop: 30,alignSelf: 'center'}}>Please accept or reject entry</Text>
+            <Text style = {{marginTop: 30,alignSelf:'center'}}>Please accept or reject entry</Text>
             
             <Button
                 onPress = { async function(){
@@ -129,8 +102,6 @@ const NotificationScreen = function({navigation}){
                 title="Reject"
                 buttonStyle={{ backgroundColor: '#000', margin: 15 }}
             />
-
-           
         </View>
     )
 }
